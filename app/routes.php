b09d7139be08f409b5919cda2ca51342da80c9b0
Francisco Naranjo <francisco.naranjo@outlook.com>
2014-05-31 21:04:48 -0500

    Asignar el filtro cumpleanos a la ruta home de la aplicación

--- app/routes.php
+++ app/routes.php
@@ -2,16 +2,13 @@
 
 /*
 |--------------------------------------------------------------------------
+| Programador: Francisco Naranjo - MacBookaro
+| Fecha: 2014/05/31 07:38 p.m.
+|
 | Application Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', function() { return View::make('hello'); });
+
+Route::get('/', array('before' => 'navidad|cumpleanos', function() { return View::make('hola'); }));
