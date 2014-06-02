617883a289306a3dd9072aae3c74ad98ae4bd46e
Francisco Naranjo <francisco.naranjo@outlook.com>
2014-06-02 15:20:33 -0500

    Formularios, campos de texto, textarea y password

--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -1,5 +1,14 @@
 <!-- app/views/form.blade.php -->
 
-{{ Form::open(array('action' => 'miControlador@miAccion')) }}
+{{ Form::open(array('url' => 'ruta/destino')) }}
+    <br>
+    {{ Form::label('txtNombre', 'Nombre', ['id' => 'lblNombre', 'name' => 'lblNombre']) }}
+    {{ Form::text('nombre', 'Taylor Otwell', ['id' => 'txtNombre', 'name' => 'txtNombre']) }}
+    <br>
+    {{ Form::label('txtDescripcion', 'Descripción', ['name' => 'lblDescripcion']) }}
+    {{ Form::textarea('txtDescripcion', '¡El mejor campo!', ['cols' => 20]) }}
+    <br>
+    {{ Form::label('secreto', 'Súper Secreto') }}
+    {{ Form::password('secreto') }}
 
 {{ Form::close() }}
