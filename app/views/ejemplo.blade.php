086edbc88fe7f93781bf87cb4504976ca37d2c6f
Francisco Naranjo <francisco.naranjo@outlook.com>
2014-06-01 19:37:34 -0500

    Estructuras de control de Blade

--- app/views/ejemplo.blade.php
+++ app/views/ejemplo.blade.php
@@ -4,3 +4,11 @@
 <?php //echo $ardilla; ?>
 <p>{{ date('d/m/y') }}</p>
 <p>{{{ '<script>alert("¡BACON APESTOSO!");</script>' }}}</p>
+
+@if ('Panda Rojo' == 'Panda Rojo')
+<p>¡Algo es rojo, blanco y marrón!</p>
+@elseif ('Panda Gigante' == 'Panda Gigante')
+<p>¡Ago es blanco y negro!</p>
+@else
+<p>Algo podría ser una ardilla.</p>
+@endif
